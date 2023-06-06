21224e5bbac17e0bef0d1364412d8e7ebf7dc9c8
Kyle Crawford <kcraw217@gmail.com>
2023-06-06 15:20:39 -0700

    add Post for new app user
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>
    dquote> co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>"

--- ARatsLifeClient/Models/ApiHelper.cs
+++ ARatsLifeClient/Models/ApiHelper.cs
@@ -12,5 +12,17 @@ namespace ARatsLifeClient.Models
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
+
+    //--------------------------- 
+    // for application users
+
+    public static async void Post(string newApplicationUser)
+    {
+      RestClient client = new RestClient("http://localhost:5102/");
+      RestRequest request = new RestRequest($"api/accounts", Method.Post);
+      request.AddHeader("Content-Type", "application/json");
+      request.AddJsonBody(newApplicationUser);
+      await client.PostAsync(request);
+    }
   }
 }
