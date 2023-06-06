686b6d29a7df2ffc8d133cfa3cf1bee1e4cde112
Kyle Crawford <kcraw217@gmail.com>
2023-06-06 15:19:59 -0700

    remove unecessary code
    co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>
    dquote> co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>"

--- ARatsLifeClient/Program.cs
+++ ARatsLifeClient/Program.cs
@@ -1,16 +1,40 @@
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// builder.Services.AddAuthentication(options =>
+// {
+//     options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+//     options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+//     options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+// }).AddJwtBearer(o =>
+// {
+//     o.TokenValidationParameters = new TokenValidationParameters
+//     {
+//         ValidIssuer = builder.Configuration["Jwt:Issuer"],
+//         ValidAudience = builder.Configuration["Jwt:Audience"],
+//         IssuerSigningKey = new SymmetricSecurityKey
+//         (Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+//         ValidateIssuer = true,
+//         ValidateAudience = true,
+//         ValidateLifetime = false,
+//         ValidateIssuerSigningKey = true
+//     };
+// });
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Home/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
